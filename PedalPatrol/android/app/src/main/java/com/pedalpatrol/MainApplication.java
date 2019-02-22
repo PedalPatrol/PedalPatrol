6cc8fdda19c06f2442e71829f84f6ca97651eef3
SeanRemedios <14sr84@queensu.ca>
2019-02-22 16:18:24 -0500

    Fixed Android crashing on emulator. TODO : Test and change views for Android. Clear button broken at AddBikeView on Android

--- PedalPatrol/android/app/src/main/java/com/pedalpatrol/MainApplication.java
+++ PedalPatrol/android/app/src/main/java/com/pedalpatrol/MainApplication.java
@@ -6,9 +6,6 @@ import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.imagepicker.ImagePickerPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -29,9 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new ImagePickerPackage(),
-            new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
             new RNGestureHandlerPackage()
