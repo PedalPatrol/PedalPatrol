52c7136646b1927f3cb7f4c34613266d7ff43643
SeanRemedios <14sr84@queensu.ca>
2019-03-06 14:31:56 -0500

    Fixed android building

--- PedalPatrol/android/app/src/main/java/com/pedalpatrol/MainApplication.java
+++ PedalPatrol/android/app/src/main/java/com/pedalpatrol/MainApplication.java
@@ -3,7 +3,6 @@ package com.pedalpatrol;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFirebasePackage(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
             new RNGestureHandlerPackage()
