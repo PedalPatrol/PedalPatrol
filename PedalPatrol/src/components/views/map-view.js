6e746d2d25203562599c3789b0291fefe7f6158e
SeanRemedios <14sr84@queensu.ca>
2019-03-17 21:07:39 -0400

    Fixed map view error

--- PedalPatrol/src/components/views/map-view.js
+++ PedalPatrol/src/components/views/map-view.js
@@ -268,16 +268,19 @@ class MapView extends BaseView {
 	_renderCallout = (item) => (
 		<Callout>
 			<View style={{flexDirection: 'row'}}>
-				<View style={{justifyContent: 'flex-start'}}
-				<Text style={styles.model} numberOfLines={1} ellipsizeMode ={'tail'}>
-					{item.data.model}
-				</Text>
+				<View style={{justifyContent: 'flex-start'}}>
+					<Text style={styles.model} numberOfLines={1} ellipsizeMode ={'tail'}>
+						{item.data.model}
+					</Text>
 				</View>
 				{/*console.log(item.data)*/}
-				<View style={{justifyContent: 'flex-end'}}
-				<Text style={styles.time} numberOfLines={1} ellipsizeMode ={'tail'}>
-					{item.data.timeago}
+				<Text>
+				{'      '}
 				</Text>
+				<View style={{justifyContent: 'flex-end'}}>
+					<Text style={styles.time} numberOfLines={1} ellipsizeMode ={'tail'}>
+						{item.data.timeago}
+					</Text>
 				</View>
 			</View>
 		</Callout>
